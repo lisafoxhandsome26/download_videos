59179f05614a3a13f441b4b9c4822d29b1488725
Alexandr <sashasergeevasus345@gmail.com>
2025-08-18 10:39:37 +0300

    The GUI interface has been improved, the video upload feature has been added, and exceptions and possible program crashes have been handled

--- back_down.py
+++ back_down.py
@@ -1,24 +1,32 @@
-from __future__ import unicode_literals
 import os
 import shutil
 import yt_dlp as youtube_dl
 from pydub import AudioSegment
 import re
+from tkinter import messagebox
 
 
-def audio_download(url_video_youtube: str, ydl_opts: dict):
-
+def audio_video_download(url_video_youtube: str, ydl_opts: dict, queue, steps=2, video=False):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        queue.put(100 / steps)
         ydl.download([url_video_youtube])
+        queue.put(100 / steps)
 
+    if video:
+        messagebox.showinfo("Готово!", "Видео успешно загружено 🎉")
+    else:
+        print(f'{"-" * 50}\nDownload Successfully!\n{"-" * 50}')
+
+
+def get_info_by_video(ydl_opts: dict, url_video_youtube: str):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url_video_youtube, download=False)
         audio_time_line = info_dict.get("chapters", None)
         new_name = info_dict.get("title", None) + ".mp3"
-        print(f'{"-" * 50}\nDownload audio Successfully!\n{"-" * 50}')
     return audio_time_line, new_name
 
 
-def audio_redactor(file_mp3_path, audio_path, time_line: list):
+def audio_redactor(file_mp3_path: str, audio_path: str, time_line: list, steps: int, queue):
     time_line_quantity = len(time_line)
     now = 0
     for line in time_line:
@@ -37,55 +45,48 @@ def audio_redactor(file_mp3_path, audio_path, time_line: list):
             extract.export(audio_path + f'\{file_name_to}.mp3', format="mp3")
             print(f'{"-" * 50}\n#{line["title"]} Created\n{"-" * 50}')
         except Exception as ex:
-            return "Ошибка", f"{'-' * 50}\n Произошла ошибка в файле {file_mp3_path}\n {ex} \n{'-' * 50}"
+            return "Error", f"{'-' * 50}\n Произошла ошибка в файле {file_mp3_path}\n {ex} \n{'-' * 50}"
 
+        queue.put(100 / steps)
     os.remove(file_mp3_path)
-    return "Информация", "Загрузка всех аудиофайлов успешно завершена 🎉🎉🎉"
+    return "Готово!", "Загрузка всех аудиофайлов успешно завершена 🎉🎉🎉"
 
 
-def move_files(destination_folder, file_name, new_name):
+def move_files(destination_folder: str, file_name: str, new_name: str):
     source_folder = "."
-    os.rename(file_name, new_name)
+    title_name = re.sub(r'[^\w\s\.-]', '', new_name)
+    os.rename(file_name, title_name)
 
-    source_path = os.path.join(source_folder, new_name)
-    destination_path = os.path.join(destination_folder, new_name)
+    source_path = os.path.join(source_folder, title_name)
+    destination_path = os.path.join(destination_folder, title_name)
 
     try:
         shutil.move(source_path, destination_path)
-        return "Информация", f"Файл успешно скачан в '{destination_folder}' 🎉"
+        return "Готово!", f"Файл успешно скачан в '{destination_folder}' 🎉"
     except FileNotFoundError:
-        return "Ошибка", f"Ошибка: Файл или папка не найдены. ❌"
+        return "Error", f"Ошибка: Файл или папка не найдены. ❌"
     except Exception as e:
-        return "Ошибка", f"Произошла ошибка: {e} ❌"
+        return "Error", f"Произошла ошибка: {e} ❌"
 
 
-def main(ydl_opts, url, path_audio_files):
+def main(url, path_audio_files, ydl_opts, queue):
     file_name = "audioDownload.mp3"
 
     if not os.path.exists(path_audio_files):
-        return "Ошибка", "Указанный путь к папке для сохранения аудиофайлов не существует!"
+        messagebox.showerror("Error", "Указанный путь к папке для сохранения аудиофайлов не существует!")
+        exit(0)
 
-    audio_time_line, new_name = audio_download(url, ydl_opts)
+    audio_time_line, new_name = get_info_by_video(ydl_opts, url)
 
     if audio_time_line:
-        message = audio_redactor(file_name, path_audio_files, audio_time_line)
+        steps = len(audio_time_line) + 2
+        audio_video_download(url, ydl_opts, queue, steps)
+        message = audio_redactor(file_name, path_audio_files, audio_time_line, steps, queue)
     else:
+        audio_video_download(url, ydl_opts, queue)
         message = move_files(path_audio_files, file_name, new_name)
 
-    return message
-
-
-# if __name__ == "__main__":
-#     ydl_opts = {
-#         'format': 'bestaudio/best',
-#         'outtmpl': 'audioDownload',
-#         'noplaylist': True,
-#         'postprocessors': [{
-#             'key': 'FFmpegExtractAudio',
-#             'preferredcodec': 'mp3',
-#             'preferredquality': '192',
-#         }],
-#     }
-#     url = input("Введите ссылку для скачивания видео ")
-#     path_audio_files = input("Введите путь к папке для сохранения аудиофайлов ")
-#     main(ydl_opts, url, path_audio_files)
+    if message[0] == "Error":
+        messagebox.showerror(message[0], message[1])
+    else:
+        messagebox.showinfo(message[0], message[1])
--- download_files.py
+++ download_files.py
@@ -1,62 +1,113 @@
 import tkinter as tk
-from tkinter import messagebox
-from back_down import main
+import threading
+import queue
+from tkinter import ttk
+from back_down import main, audio_video_download
 
 
-class App(tk.Frame):
-    def __init__(self, master):
-        super().__init__(master)
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Загрузка Аудио / Видео")
+        self.geometry(f"{width}x{height}")
+        self.resizable(False, False)
 
-        self.pack()
+        self.config(bg="#C7FCEC")
 
         self.entry_link = tk.Entry(width=int(int(width * 0.8) / 8))
         self.entry_link.pack(pady=(10, 5))
-
         self.link_download = tk.StringVar()
         self.link_download.set("Ссылка для скачивания")
         self.entry_link["textvariable"] = self.link_download
 
         self.entry_path = tk.Entry(width=int(int(width * 0.8) / 8))
         self.entry_path.pack(pady=(5, 5))
-
         self.path_download = tk.StringVar()
-        self.path_download.set("Путь для сохранения файлов")
+        self.path_download.set("E:\downloads youtube")
         self.entry_path["textvariable"] = self.path_download
 
-        button = tk.Button(text="Выполнить", width=15, height=10, command=self.print_contents)
-        button.pack(pady=10)
+        # Очередь для обмена данными между потоком и GUI
+        self.queue = queue.Queue()
 
-    def print_contents(self):
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'outtmpl': 'audioDownload',
-            'noplaylist': True,
-            'postprocessors': [{
+        # Прогрессбар
+        self.progressbar = ttk.Progressbar(self, length=300, orient=tk.HORIZONTAL, mode='determinate', maximum=100)
+        self.progressbar.pack(padx=10, pady=10)
+
+        # Кнопки запуска скачивания Аудио и Видео
+        frame = tk.Frame(self, bg="#C7FCEC")
+        frame.pack(expand=True)
+        self.button_audio = tk.Button(frame,
+                                      text="Музыка",
+                                      width=10,
+                                      height=2,
+                                      bg="#b9f264",
+                                      command=self.start_action_audio)
+        self.button_video = tk.Button(frame,
+                                      text="Видео",
+                                      width=10,
+                                      height=2,
+                                      bg="#b9f264",
+                                      command=self.start_action_video)
+
+        self.button_audio.pack(side=tk.LEFT, padx=(0, 10))
+        self.button_video.pack(side=tk.LEFT)
+
+    def prepare_actions(self):
+        self.button_video.config(state=tk.DISABLED)
+        self.button_audio.config(state=tk.DISABLED)
+        self.progressbar['value'] = 0
+        link = self.link_download.get()
+        path = self.path_download.get()
+        return link, path
+
+    def start_action_audio(self):
+        link, path = self.prepare_actions()
+
+        ydl_opts['format'] = 'bestaudio/best'
+        ydl_opts['outtmpl'] = 'audioDownload'
+        ydl_opts['postprocessors'] = [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
-            }],
-        }
-        link = self.link_download.get()
-        path = self.path_download.get()
-        message = main(ydl_opts, link, path)
-        print(message)
-        if message[0] == "Информация":
-            messagebox.showinfo(message[0], message[1])
+            }]
+
+        # Запуск фоновой задачи в отдельном потоке
+        thread = threading.Thread(target=main, args=(link, path, ydl_opts, self.queue))
+        thread.start()
+        self.after(100, lambda: self.poll_thread(thread))
+
+    def start_action_video(self):
+        link, path = self.prepare_actions()
+
+        ydl_opts['format'] = 'bestvideo+bestaudio/best'
+        ydl_opts['outtmpl'] = f'{path}/%(title)s.%(ext)s'
+        ydl_opts.pop('postprocessors', None)
+
+        video, steps = True, 2
+        thread = threading.Thread(target=audio_video_download, args=(link, ydl_opts, self.queue, steps, video))
+        thread.start()
+        self.after(100, lambda: self.poll_thread(thread))
+
+    def poll_thread(self, thread):
+        self.check_queue()
+        if thread.is_alive():
+            self.after(100, lambda: self.poll_thread(thread))
         else:
-            messagebox.showerror(message[0], message[1])
+            self.button_audio.config(state=tk.NORMAL)
+            self.button_video.config(state=tk.NORMAL)
 
-# E:\downloads youtube
-if __name__ == "__main__":
+    def check_queue(self):
+        while not self.queue.empty():
+            step = self.queue.get()
+            self.progressbar.step(step)
 
-    width = 600
-    height = 120
 
-    root = tk.Tk()
-    root.title("Загрузчик видео")
-    root.geometry(f"{width}x{height}")
-    root.resizable(False, False)
+width = 600
+height = 160
 
-    myapp = App(root)
+ydl_opts = {
+    'noplaylist': True,
+}
 
-    myapp.mainloop()
+app = App()
+app.mainloop()
